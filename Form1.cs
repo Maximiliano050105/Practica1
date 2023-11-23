1c981692054a446dea141de8457cab91d3c8c1a8
max cuevas <max cuevas@DESKTOP-24TMU4R>
2023-11-22 19:07:47 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+namespace Practica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal;
+            cal = double.Parse(textBox1.Text);
+            if (cal < 3.0) MessageBox.Show("Reprobado");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
